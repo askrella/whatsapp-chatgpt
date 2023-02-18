9e0dfda83a4869f899085cef4e7d5d9d852cadb9
Justice Ekemezie <ttebify@gmail.com>
2023-02-18 13:59:50 -0800

    includes a loading spinner to improve the UX

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,107 @@
-const process = require("process")
+const process = require("process");
 const qrcode = require("qrcode-terminal");
 const { Client } = require("whatsapp-web.js");
-import { ChatGPTAPI, ChatMessage } from 'chatgpt'
+import { ChatGPTAPI, ChatMessage } from 'chatgpt';
 
 // Environment variables
-require("dotenv").config()
+require("dotenv").config();
 
 // Prefix check
-const prefixEnabled = process.env.PREFIX_ENABLED == "true"
-const prefix = '!gpt'
+const prefixEnabled = process.env.PREFIX_ENABLED == "true";
+const prefix = '!gpt';
 
-// Whatsapp Client
-const client = new Client()
+// WhatsApp Client
+const client = new Client();
 
 // ChatGPT Client
 const api = new ChatGPTAPI({
     apiKey: process.env.OPENAI_API_KEY
-})
+});
 
 // Mapping from number to last conversation id
-const conversations = {}
+const conversations = {};
 
-// Entrypoint
+// Loading spinner
+const loadingSpinner = [
+  "|",
+  "/",
+  "-",
+  "\\"
+];
+let loadingSpinnerIndex = 0;
+
+// Entry point
 const start = async () => {
-    // Whatsapp auth
+    // WhatsApp auth
     client.on("qr", (qr: string) => {
-        console.log("[Whatsapp ChatGPT] Scan this QR code in whatsapp to log in:")
+        console.log("[WhatsApp ChatGPT] Scan this QR code in WhatsApp to log in:");
         qrcode.generate(qr, { small: true });
-    })
+    });
 
-    // Whatsapp ready
+    // WhatsApp ready
     client.on("ready", () => {
-        console.log("[Whatsapp ChatGPT] Client is ready!");
-    })
+        console.log("[WhatsApp ChatGPT] Client is ready!");
+    });
 
-    // Whatsapp message
+    // WhatsApp message
     client.on("message", async (message: any) => {
-        if (message.body.length == 0) return
-        if (message.from == "status@broadcast") return
+        if (message.body.length == 0) return;
+        if (message.from == "status@broadcast") return;
 
         if (prefixEnabled) {
             if (message.body.startsWith(prefix)) {
                 // Get the rest of the message
                 const prompt = message.body.substring(prefix.length + 1);
-                await handleMessage(message, prompt)
+                await handleMessage(message, prompt);
             }
         } else {
-            await handleMessage(message, message.body)
+            await handleMessage(message, message.body);
         }
-    })
+    });
 
-    client.initialize()
-}
+    client.initialize();
+};
 
 const handleMessage = async (message: any, prompt: any) => {
     try {
-        const lastConversation = conversations[message.from]
+        const lastConversation = conversations[message.from];
 
         // Add the message to the conversation
-        console.log("[Whatsapp ChatGPT] Received prompt from " + message.from + ": " + prompt)
+        console.log("[WhatsApp ChatGPT] Received prompt from " + message.from + ": " + prompt);
+
+        // Display the loading spinner while the chatbot generates a response
+        const loadingSpinnerInterval = setInterval(() => {
+          process.stdout.write(`\r[WhatsApp ChatGPT] ChatGPT is thinking ${loadingSpinner[loadingSpinnerIndex]}`);
+          loadingSpinnerIndex = (loadingSpinnerIndex + 1) % loadingSpinner.length;
+        }, 100);
+
         let response: ChatMessage;
 
-        const start = Date.now()
+        const start = Date.now();
         if (lastConversation) {
-            response = await api.sendMessage(prompt, lastConversation)
+            response = await api.sendMessage(prompt, lastConversation);
         } else {
-            response = await api.sendMessage(prompt)
+            response = await api.sendMessage(prompt);
         }
-        const end = Date.now() - start
+        const end = Date.now() - start;
 
-        console.log(`[Whatsapp ChatGPT] Answer to ${message.from}: ${response.text}`)
+        clearInterval(loadingSpinnerInterval);
+        process.stdout.write(`\r[WhatsApp ChatGPT] ChatGPT took ${end}ms to generate the response.      \n`);
+
+        console.log(`[WhatsApp ChatGPT] Answer to ${message.from}: ${response.text}`);
 
         // Set the conversation
         conversations[message.from] = {
             conversationId: response.conversationId,
             parentMessageId: response.id
-        }
-
-        console.log("[Whatsapp ChatGPT] ChatGPT took " + end + "ms")
+        };
 
         // Send the response to the chat
-        message.reply(response.text)
+        message.reply(response.text);
     } catch (error: any) {
-        console.error("An error occured", error)
-        message.reply("An error occured, please contact the administrator. (" + error.message + ")")
+        console.error("An error occured", error);
+        message.reply("An error occured, please contact the administrator. (" + error.message + ")");
     }
-}
+};
 
-start()
+start();
