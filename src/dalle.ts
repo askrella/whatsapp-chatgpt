53dc786ff2127c33f5df88fe2394e158914df84f
Stanislav Hetzel <stanislav.hetzel@outlook.com>
2023-02-19 14:32:50 +0100

    Fix typescript base64 (string) in dalle.ts

--- src/dalle.ts
+++ src/dalle.ts
@@ -17,7 +17,7 @@ const handleMessageDALLE = async (message: any, prompt: any) => {
 
         const end = Date.now() - start
 
-        const base64 = response.data.data[0].b64_json;
+        const base64 = response.data.data[0].b64_json as string;
         const image = await new MessageMedia("image/jpeg", base64, "image.jpg");
 
         console.log(`[Whatsapp DALLE] Answer to ${message.from} | OpenAI request took ${end}ms`)
