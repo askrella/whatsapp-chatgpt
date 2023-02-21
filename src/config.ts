4154d245727507414371e5e7d3e7073b2d28dd0f
Stanislav Hetzel <stanislav.hetzel@outlook.com>
2023-02-21 23:26:01 +0100

    Fix env (PREFIX_ENABLED)

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,12 @@ import process from "process";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Interface
+// Config Interface
 interface IConfig {
+	// OpenAI API Key
 	openAIAPIKey: string;
+
+	// Prefix
 	prefixEnabled: boolean;
 	gptPrefix: string;
 	dallePrefix: string;
@@ -16,10 +19,19 @@ interface IConfig {
 // Config
 const config: IConfig = {
 	openAIAPIKey: process.env.OPENAI_API_KEY || "", // Default: ""
-	prefixEnabled: process.env.PREFIX_ENABLED == "true" || true, // Default: true
-	gptPrefix: process.env.GPT_PREFIX ||  "!gpt", // Default: !gpt
+
+	// Prefix
+	prefixEnabled: getEnvBooleanWithDefault("PREFIX_ENABLED", true), // Default: true
+	gptPrefix: process.env.GPT_PREFIX || "!gpt", // Default: !gpt
 	dallePrefix: process.env.DALLE_PREFIX || "!dalle", // Default: !dalle
 	aiConfigPrefix: process.env.AI_CONFIG_PREFIX || "!config", // Default: !config
-};
+}
+
+function getEnvBooleanWithDefault(key: string, defaultValue: boolean): boolean {
+	if (process.env[key] == undefined || process.env[key] == "") {
+		return defaultValue;
+	}
+	return process.env[key] == "true";
+}
 
 export default config;
