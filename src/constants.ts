69aba57b37ae18fc552bb6621d41df5e03d107d5
byt3magic <96043826+byt3magic@users.noreply.github.com>
2023-02-21 01:26:41 +0100

    Added feature to store sessions automatically

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,13 @@
 interface IConstants {
 	// WhatsApp status broadcast
 	statusBroadcast: string;
+	// WhatsApp session storage
+	sessionData: string;
 }
 
 const constants: IConstants = {
-	statusBroadcast: "status@broadcast"
+	statusBroadcast: "status@broadcast",
+	sessionData: "./session/session.json"
 };
 
 export default constants;
