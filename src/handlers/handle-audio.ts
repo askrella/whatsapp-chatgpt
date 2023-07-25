171bc604f51104b7f566267353ff660440706026
Luandro <luandro@digital-democracy.org>
2023-07-25 09:23:10 -0300

    feat(handle-audio.ts): add new file handle-audio.ts to handle audio messages and transcribe them refactor(message.ts): remove audio transcription logic from handleIncomingMessage function and call handleAudio function to handle audio messages

---
+++ src/handlers/handle-audio.ts
@@ -0,0 +1,79 @@
+// Speech API & Whisper
+import { TranscriptionMode } from "../types/transcription-mode";
+import { transcribeRequest } from "../providers/speech";
+import { transcribeAudioLocal } from "../providers/whisper-local";
+import { transcribeWhisperApi } from "../providers/whisper-api";
+import { transcribeOpenAI } from "../providers/openai";
+import { getConfig } from "../handlers/ai-config";
+import * as cli from "../cli/ui";
+import { Message } from "whatsapp-web.js";
+import { checkAction } from "../handlers/check-action";
+import { handleMessageGPT, handleDeleteConversation } from "../handlers/gpt";
+import { handleMessageDALLE } from "../handlers/dalle";
+
+type Media = {
+    data: string;
+  };
+
+export async function handleAudio (media: Media, message: Message): Promise<void> {
+    // Check if transcription is enabled (Default: false)
+		if (!getConfig("transcription", "enabled")) {
+			cli.print("[Transcription] Received voice messsage but voice transcription is disabled.");
+			return;
+		}
+
+		// Convert media to base64 string
+		const mediaBuffer = Buffer.from(media.data, "base64");
+
+		// Transcribe locally or with Speech API
+		const transcriptionMode = getConfig("transcription", "mode");
+		cli.print(`[Transcription] Transcribing audio with "${transcriptionMode}" mode...`);
+
+		let res;
+		switch (transcriptionMode) {
+			case TranscriptionMode.Local:
+				res = await transcribeAudioLocal(mediaBuffer);
+				break;
+			case TranscriptionMode.OpenAI:
+				res = await transcribeOpenAI(mediaBuffer);
+				break;
+			case TranscriptionMode.WhisperAPI:
+				res = await transcribeWhisperApi(new Blob([mediaBuffer]));
+				break;
+			case TranscriptionMode.SpeechAPI:
+				res = await transcribeRequest(new Blob([mediaBuffer]));
+				break;
+			default:
+				cli.print(`[Transcription] Unsupported transcription mode: ${transcriptionMode}`);
+		}
+		const { text: transcribedText, language: transcribedLanguage } = res;
+
+		// Check transcription is null (error)
+		if (transcribedText == null) {
+			message.reply("I couldn't understand what you said.");
+			return;
+		}
+
+		// Check transcription is empty (silent voice message)
+		if (transcribedText.length == 0) {
+			message.reply("I couldn't understand what you said.");
+			return;
+		}
+
+		// Log transcription
+		cli.print(`[Transcription] Transcription response: ${transcribedText} (language: ${transcribedLanguage})`);
+
+		// Reply with transcription
+		// const reply = `You said: ${transcribedText}${transcribedLanguage ? " (language: " + transcribedLanguage + ")" : ""}`;
+		const actionType = await checkAction(message, transcribedText)
+		console.log('ACTION TYPE', actionType)
+		switch (actionType) {
+			case 'image':
+				await handleMessageDALLE(message, transcribedText);
+				return;
+			case 'gpt':
+			default:
+				await handleMessageGPT(message, transcribedText);
+				return;
+		}
+}
--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -8,7 +8,7 @@ import config from "../config";
 import * as cli from "../cli/ui";
 
 // Custom
-import { checkAction } from "../handlers/check-action";
+import { handleAudio } from "../handlers/handle-audio";
 
 
 // ChatGPT & DALLE
@@ -17,12 +17,6 @@ import { handleMessageDALLE } from "../handlers/dalle";
 import { handleMessageAIConfig, getConfig, executeCommand } from "../handlers/ai-config";
 import { handleMessageLangChain } from "../handlers/langchain";
 
-// Speech API & Whisper
-import { TranscriptionMode } from "../types/transcription-mode";
-import { transcribeRequest } from "../providers/speech";
-import { transcribeAudioLocal } from "../providers/whisper-local";
-import { transcribeWhisperApi } from "../providers/whisper-api";
-import { transcribeOpenAI } from "../providers/openai";
 
 // For deciding to ignore old messages
 import { botReadyTimestamp } from "../index";
@@ -65,71 +59,12 @@ async function handleIncomingMessage(message: Message) {
 	// Transcribe audio
 	if (message.hasMedia) {
 		const media = await message.downloadMedia();
-
-		// Ignore non-audio media
-		if (!media || !media.mimetype.startsWith("audio/")) return;
-
-		// Check if transcription is enabled (Default: false)
-		if (!getConfig("transcription", "enabled")) {
-			cli.print("[Transcription] Received voice messsage but voice transcription is disabled.");
-			return;
-		}
-
-		// Convert media to base64 string
-		const mediaBuffer = Buffer.from(media.data, "base64");
-
-		// Transcribe locally or with Speech API
-		const transcriptionMode = getConfig("transcription", "mode");
-		cli.print(`[Transcription] Transcribing audio with "${transcriptionMode}" mode...`);
-
-		let res;
-		switch (transcriptionMode) {
-			case TranscriptionMode.Local:
-				res = await transcribeAudioLocal(mediaBuffer);
-				break;
-			case TranscriptionMode.OpenAI:
-				res = await transcribeOpenAI(mediaBuffer);
-				break;
-			case TranscriptionMode.WhisperAPI:
-				res = await transcribeWhisperApi(new Blob([mediaBuffer]));
-				break;
-			case TranscriptionMode.SpeechAPI:
-				res = await transcribeRequest(new Blob([mediaBuffer]));
-				break;
-			default:
-				cli.print(`[Transcription] Unsupported transcription mode: ${transcriptionMode}`);
-		}
-		const { text: transcribedText, language: transcribedLanguage } = res;
-
-		// Check transcription is null (error)
-		if (transcribedText == null) {
-			message.reply("I couldn't understand what you said.");
-			return;
-		}
-
-		// Check transcription is empty (silent voice message)
-		if (transcribedText.length == 0) {
-			message.reply("I couldn't understand what you said.");
-			return;
-		}
-
-		// Log transcription
-		cli.print(`[Transcription] Transcription response: ${transcribedText} (language: ${transcribedLanguage})`);
-
-		// Reply with transcription
-		// const reply = `You said: ${transcribedText}${transcribedLanguage ? " (language: " + transcribedLanguage + ")" : ""}`;
-		const actionType = await checkAction(message, transcribedText)
-		console.log('ACTION TYPE', actionType)
-		switch (actionType) {
-			case 'image':
-				await handleMessageDALLE(message, transcribedText);
+		if (media) {
+			if (media.mimetype.startsWith("audio/")) {
+				handleAudio(media, message);
 				return;
-			case 'gpt':
-			default:
-				await handleMessageGPT(message, transcribedText);
-				return;
-		}
-
+			} else return;
+		} else return;
 	}
 
 	// Clear conversation context (!clear)
